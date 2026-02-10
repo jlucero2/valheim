cf1996c4bd147ae36fd623bdffa994ac68dc0986
jordi <3315258+jlucero2@users.noreply.github.com>
2026-02-10 20:06:05 +0000

    update server start modifiers

--- custom_server_start.sh
+++ custom_server_start.sh
@@ -8,7 +8,7 @@ echo "Starting server PRESS CTRL-C to exit"
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "BonkTown" -port 2456 -world "BonkTown" -password "buttstuff42069" -logFile log.txt -savedir /home/steam/valheimsave -public 0 -modifier raids less -modifier resources double -modifier portals casual -modifier death casual
+./valheim_server.x86_64 -name "BonkTown" -port 2456 -world "BonkTown" -password "buttstuff42069" -logFile log.txt -savedir /home/steam/valheimsave -public 0 -modifier raids less -modifier resources muchmore -modifier portals casual -modifier deathpenalty casual -modifier combat easy
 
 export LD_LIBRARY_PATH=$templdpath
 
